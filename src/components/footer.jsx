b81273b220a08441c20bfe5e6bdf5ab74632ec15
Ikenna <iykethe1st@gmail.com>
2024-02-02 15:34:01 +0100

    Changed Project name

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,39 +2,47 @@ import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 
 function Footer() {
   return (
-    <footer className='mt-5 flex items-center justify-between rounded-t-xl border-t-2 border-t-purple bg-gray-100/80 p-5 dark:bg-txt/10 lg:py-10'>
-      <section className='basis-1/4'>
-        <p className='head bg-gradient-to-r from-sub to-purple bg-clip-text text-lg font-bold  text-transparent md:text-3xl '>
-          Cinematrix
+    <footer className="mt-5 flex items-center justify-between rounded-t-xl border-t-2 border-t-purple bg-gray-100/80 p-5 dark:bg-txt/10 lg:py-10">
+      <section className="basis-1/4">
+        <p className="head bg-gradient-to-r from-sub to-purple bg-clip-text text-lg font-bold  text-transparent md:text-3xl ">
+          VidCee
         </p>
       </section>
 
-      <p className='hidden basis-1/2 md:block text-center text-txt dark:text-white/80'>
-        All Data Sourced From <a className="text-purple" href='https://www.themoviedb.org/' target="_blank" rel="noreferrer">TheMovieDb</a>
+      <p className="hidden basis-1/2 text-center text-txt dark:text-white/80 md:block">
+        All Data Sourced From{" "}
+        <a
+          className="text-purple"
+          href="https://www.themoviedb.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          TheMovieDb
+        </a>
       </p>
 
-      <section className='flex basis-2/5 justify-around md:basis-1/4'>
+      <section className="flex basis-2/5 justify-around md:basis-1/4">
         <a
-          className='text-txt transition-all duration-300 ease-linear hover:text-purple dark:text-white/80 dark:hover:text-purple'
-          href='https://github.com/Ony3dika'
-          target='_blank'
-          rel='noreferrer'
+          className="text-txt transition-all duration-300 ease-linear hover:text-purple dark:text-white/80 dark:hover:text-purple"
+          href="https://github.com/Ony3dika"
+          target="_blank"
+          rel="noreferrer"
         >
           <FaGithub size={"1.4rem"} />
         </a>
         <a
-          className='text-txt transition-all duration-300 ease-linear hover:text-purple dark:text-white/80 dark:hover:text-purple'
-          href='https://twitter.com/Ony3dikaa/'
-          target='_blank'
-          rel='noreferrer'
+          className="text-txt transition-all duration-300 ease-linear hover:text-purple dark:text-white/80 dark:hover:text-purple"
+          href="https://twitter.com/Ony3dikaa/"
+          target="_blank"
+          rel="noreferrer"
         >
           <FaTwitter size={"1.4rem"} />
         </a>
         <a
-          className='text-txt transition-all duration-300 ease-linear hover:text-purple dark:text-white/80 dark:hover:text-purple'
-          href='https://www.linkedin.com/in/onyedikachukwu-akpa-127360248'
-          target='_blank'
-          rel='noreferrer'
+          className="text-txt transition-all duration-300 ease-linear hover:text-purple dark:text-white/80 dark:hover:text-purple"
+          href="https://www.linkedin.com/in/onyedikachukwu-akpa-127360248"
+          target="_blank"
+          rel="noreferrer"
         >
           <FaLinkedin size={"1.4rem"} />
         </a>
@@ -43,4 +51,4 @@ function Footer() {
   );
 }
 
-export default Footer
+export default Footer;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -19,11 +19,14 @@ function Navbar() {
   }, [theme]);
 
   return (
-    <nav className='sticky top-0 z-20 bg-primary px-2 dark:bg-primaryDark'>
-      <div className='flex py-5'>
-        <section className='flex basis-full justify-between lg:basis-2/3'>
-          <Link to={"/"} className='head bg-gradient-to-r from-sub to-purple bg-clip-text text-3xl font-bold text-transparent '>
-            Cinematrix
+    <nav className="sticky top-0 z-20 bg-primary px-2 dark:bg-primaryDark">
+      <div className="flex py-5">
+        <section className="flex basis-full justify-between lg:basis-2/3">
+          <Link
+            to={"/"}
+            className="head bg-gradient-to-r from-sub to-purple bg-clip-text text-3xl font-bold text-transparent "
+          >
+            VidCee
           </Link>
 
           <div>
@@ -37,27 +40,27 @@ function Navbar() {
           </div>
         </section>
         {/* Links */}
-        <section className='hidden basis-5/6 items-center justify-between lg:flex'>
+        <section className="hidden basis-5/6 items-center justify-between lg:flex">
           {Path.map((item, index) => (
             <Link to={item.link} key={index}>
               <button className='after:content-[" "] relative flex items-center px-3 py-1.5 text-sub/70 transition-all duration-300 ease-linear after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-purple after:transition-[0.3s] hover:text-purple hover:after:w-full hover:after:duration-500'>
-                {item.icon} <p className='ml-1'>{item.title}</p>
+                {item.icon} <p className="ml-1">{item.title}</p>
               </button>
             </Link>
           ))}
         </section>
 
-        <section className='hidden items-center justify-end lg:flex lg:basis-1/3'>
+        <section className="hidden items-center justify-end lg:flex lg:basis-1/3">
           <button
-            className='flex items-center transition-all duration-300 ease-linear'
+            className="flex items-center transition-all duration-300 ease-linear"
             onClick={() => {
               setTheme(theme === "dark" ? "light" : "dark");
             }}
           >
             {theme === "dark" ? (
-              <MdSunny className='text-primary/80' size={"1.5rem"} />
+              <MdSunny className="text-primary/80" size={"1.5rem"} />
             ) : (
-              <MdDarkMode className='text-txt' size={"1.5rem"} />
+              <MdDarkMode className="text-txt" size={"1.5rem"} />
             )}
           </button>
         </section>
@@ -69,41 +72,41 @@ function Navbar() {
           menu ? "left-0" : "left-[-100%]"
         } `}
       >
-        <div className='mt-3 flex justify-between'>
+        <div className="mt-3 flex justify-between">
           <Link to={"/"}>
-            <p className='head bg-gradient-to-r from-sub to-purple bg-clip-text text-2xl font-bold text-transparent '>
-              Cinematrix
+            <p className="head bg-gradient-to-r from-sub to-purple bg-clip-text text-2xl font-bold text-transparent ">
+              VidCee
             </p>
           </Link>
           <div>
             <MdClose
               size={"1.5rem"}
-              className='mt-1 text-purple lg:hidden'
+              className="mt-1 text-purple lg:hidden"
               onClick={() => setMenu(false)}
             />
           </div>
         </div>
         {Path.map((item, index) => (
           <Link to={item.link} key={index}>
-            <button className='my-5 flex items-center rounded-full py-1.5 text-sub md:my-10'>
-              {item.icon} <span className='ml-1'>{item.title}</span>
+            <button className="my-5 flex items-center rounded-full py-1.5 text-sub md:my-10">
+              {item.icon} <span className="ml-1">{item.title}</span>
             </button>
           </Link>
         ))}
 
         <button
-          className='flex items-center transition-all duration-300 ease-linear'
+          className="flex items-center transition-all duration-300 ease-linear"
           onClick={() => {
             setTheme(theme === "dark" ? "light" : "dark");
           }}
         >
           {theme === "dark" ? (
-            <MdSunny size={"1.3rem"} className='text-primary/80' />
+            <MdSunny size={"1.3rem"} className="text-primary/80" />
           ) : (
-            <MdDarkMode size={"1.3rem"} className='text-txt' />
+            <MdDarkMode size={"1.3rem"} className="text-txt" />
           )}
 
-          <span className='ml-1 font-medium text-sub'>Theme</span>
+          <span className="ml-1 font-medium text-sub">Theme</span>
         </button>
       </section>
     </nav>
