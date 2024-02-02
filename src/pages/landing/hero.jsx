83f79f6f5ad3061eade6053faf05befb053f7e6d
Ikenna <iykethe1st@gmail.com>
2024-02-02 15:34:58 +0100

    hot fix

--- src/pages/landing/hero.jsx
+++ src/pages/landing/hero.jsx
@@ -1,12 +1,5 @@
 import { FaGooglePlay } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import img1 from "../../assets/img1.webp";
-import img2 from "../../assets/img2.webp";
-import img3 from "../../assets/img3.webp";
-import img4 from "../../assets/img4.webp";
-import img5 from "../../assets/img5.webp";
-import img6 from "../../assets/img6.webp";
-import img7 from "../../assets/img7.webp";
 import { moviePictures } from "../../constants/store";
 import MovieCard from "../../components/common/movieCard";
 
@@ -43,7 +36,6 @@ function Hero() {
             hasMarginTop={index % 2 === 0}
             key={index}
           />
-          // Apply margin-top to every second MovieCard
         ))}
       </div>
     </div>
